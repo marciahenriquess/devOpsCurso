3efede43e45ca530efb913ffe3212e4d77906218
márcia henriques <marciahenriques@gsuite.iff.edu.br>
2022-11-03 19:32:54 -0300

    Update

---
+++ docker-lab/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]],["vagrant-winnfsd",["= 1.4.0"]]],"checksum":"cbca3ece7684a219521cac641dafc849d2e2884c18384f0b67710bde313ab250","vagrant_version":"2.3.2"}
